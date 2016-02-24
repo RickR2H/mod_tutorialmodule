6de790ae0084f6e2ae9746cc1fbf8d0692e8c489
Rick Spaan | R2H <rick@r2h.nl>
2016-02-24 21:32:37 +0100

    Added params and language

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,6 +12,16 @@ defined('_JEXEC') or die;
 
 $componenttype  = $params->get('componenttype','');
 $modid          = $module->id;
+$header1        = $params->get('header1','');
+$header2        = $params->get('header2','');
+$header3        = $params->get('header3','');
+$header4        = $params->get('header4','');
+$header5        = $params->get('header5','');
+$tutorial1      = $params->get('tutorial1','');
+$tutorial2      = $params->get('tutorial2','');
+$tutorial3      = $params->get('tutorial3','');
+$tutorial4      = $params->get('tutorial4','');
+$tutorial5      = $params->get('tutorial5','');
 
 // Get the applications params
 $jinput = JFactory::getApplication()->input;
@@ -19,68 +29,78 @@ $jinput = JFactory::getApplication()->input;
 // Get content type
 $type = $jinput->get('option');
 ?>
+
 <?php if($componenttype == $type || $componenttype == 'always') : ?>
+	<?php if(!empty($header1) && !empty($tutorial1)) : ?>
 	<div class="accordion" id="accordion<?php echo $modid; ?>">
 		<div class="accordion-group">
-			<div class="accordion-heading">
+			<div class="accordion-heading" style="background-color: #FCF8E3">
 				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion<?php echo $modid; ?>" href="#collapseTutor1">
-					<?php //echo $value[0]; ?>sdf
+					<?php echo $header1; ?>
 				</a>
 			</div>
 			<div id="collapseTutor1" class="accordion-body collapse">
 				<div class="accordion-inner">
-					<?php ///echo $value[1]; ?>wer
+					<?php echo $tutorial1; ?>
 				</div>
 			</div>
 		</div>
+		<?php endif; ?>
+		<?php if(!empty($header2) && !empty($tutorial2)) : ?>
 		<div class="accordion-group">
-			<div class="accordion-heading">
+			<div class="accordion-heading" style="background-color: #FCF8E3">
 				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion<?php echo $modid; ?>" href="#collapseTutor2">
-					<?php //echo $value[0]; ?>asd
+					<?php echo $header2; ?>
 				</a>
 			</div>
 			<div id="collapseTutor2" class="accordion-body collapse">
 				<div class="accordion-inner">
-					<?php ///echo $value[1]; ?>asd
+					<?php echo $tutorial2; ?>
 				</div>
 			</div>
 		</div>
+		<?php endif; ?>
+		<?php if(!empty($header3) && !empty($tutorial3)) : ?>
 		<div class="accordion-group">
-			<div class="accordion-heading">
+			<div class="accordion-heading" style="background-color: #FCF8E3">
 				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion<?php echo $modid; ?>" href="#collapseTutor3">
-					<?php //echo $value[0]; ?>asd
+					<?php echo $header3; ?>
 				</a>
 			</div>
 			<div id="collapseTutor3" class="accordion-body collapse">
 				<div class="accordion-inner">
-					<?php ///echo $value[1]; ?>asd
+					<?php echo $tutorial3; ?>
 				</div>
 			</div>
 		</div>
+		<?php endif; ?>
+		<?php if(!empty($header4) && !empty($tutorial4)) : ?>
 		<div class="accordion-group">
-			<div class="accordion-heading">
+			<div class="accordion-heading" style="background-color: #FCF8E3">
 				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion<?php echo $modid; ?>" href="#collapseTutor4">
-					<?php //echo $value[0]; ?>asd
+					<?php echo $header4; ?>
 				</a>
 			</div>
 			<div id="collapseTutor4" class="accordion-body collapse">
 				<div class="accordion-inner">
-					<?php ///echo $value[1]; ?>asd
+					<?php echo $tutorial4; ?>
 				</div>
 			</div>
 		</div>
+		<?php endif; ?>
+		<?php if(!empty($header5) && !empty($tutorial5)) : ?>
 		<div class="accordion-group">
-			<div class="accordion-heading">
+			<div class="accordion-heading" style="background-color: #FCF8E3">
 				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion<?php echo $modid; ?>" href="#collapseTutor5">
-					<?php //echo $value[0]; ?>asd
+					<?php echo $header5; ?>
 				</a>
 			</div>
 			<div id="collapseTutor5" class="accordion-body collapse">
 				<div class="accordion-inner">
-					<?php ///echo $value[1]; ?>asd
+					<?php echo $tutorial5; ?>
 				</div>
 			</div>
 		</div>
-
+		<?php endif; ?>
 	</div>
 <?php endif ?>
